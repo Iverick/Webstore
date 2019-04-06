76e9aaa6c7a9a4ac46913b9d0040e75fb80bad49
Iverick <iverick091@gmail.com>
2019-04-06 15:46:35 +0300

    Added basic Cart functionality.

---
+++ src/main/java/com/packt/webstore/service/impl/CartMapper.java
@@ -0,0 +1,35 @@
+package com.packt.webstore.service.impl;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+
+import com.packt.webstore.domain.Cart;
+import com.packt.webstore.service.ProductService;
+import com.packt.webstore.domain.CartItem;
+
+
+public class CartMapper implements RowMapper<Cart> {
+
+	private CartItemMapper cartItemMapper;
+	private NamedParameterJdbcTemplate jdbcTemplate;
+	
+	public CartMapper(NamedParameterJdbcTemplate jdbcTemplate, ProductService productService) {
+		this.jdbcTemplate = jdbcTemplate;
+		cartItemMapper = new CartItemMapper(productService);
+	}
+	
+	public Cart mapRow(ResultSet rs, int rowNum) throws SQLException {
+		String id = rs.getString("ID");
+		Cart cart = new Cart(id);
+		
+		String SQL = String.format("SELECT * FROM CART_ITEM WHERE CART_ID = '%s'", id);
+		List<CartItem> cartItems = jdbcTemplate.query(SQL, cartItemMapper);
+		cart.setCartItems(cartItems);
+		return cart;
+	}
+	
+}
